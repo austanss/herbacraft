8107630f02399f1d53bf7ace197255883da331cf
rizet <ryzenupp@hotmail.com>
2021-07-03 16:14:00 -0400

    feat: port dried cannabis

--- src/main/java/com/rizet/herbacraft/registries/ItemRegistry.java
+++ src/main/java/com/rizet/herbacraft/registries/ItemRegistry.java
@@ -12,12 +12,14 @@ public class ItemRegistry {
  
     public static final Item CANNABIS_SEEDS = new AliasedBlockItem(BlockRegistry.CANNABIS_PLANT, new Item.Settings().group(ItemGroup.MISC));
     public static final Item CANNABIS_FLOWER = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item DRIED_CANNABIS = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
     public static final Item FOSSILIZED_CANNABIS_SEEDS = new BlockItem(BlockRegistry.FOSSILIZED_CANNABIS_SEEDS, new Item.Settings().group(ItemGroup.DECORATIONS));
 
     public static void register() {
         Registry.register(Registry.ITEM, Herbacraft.newId("cannabis_seeds"), CANNABIS_SEEDS);
         Registry.register(Registry.ITEM, Herbacraft.newId("fossilized_cannabis_seeds"), FOSSILIZED_CANNABIS_SEEDS);
         Registry.register(Registry.ITEM, Herbacraft.newId("cannabis_flower"), CANNABIS_FLOWER);
+        Registry.register(Registry.ITEM, Herbacraft.newId("dried_cannabis"), DRIED_CANNABIS);
     }
 
 }
